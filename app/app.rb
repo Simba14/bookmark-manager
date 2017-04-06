c76e243059dea0eced92aa05639f37564dac0bc0
Colin Turner <colinturner34@gmail.com>
2017-04-06 11:48:48 +0100

    Add capability to filter links by tag

--- app/app.rb
+++ app/app.rb
@@ -23,8 +23,9 @@ class BookmarkManager < Sinatra::Base
     redirect '/links'
   end
 
-  get '/tags/bubbles' do
-    @filtered_links = Link.all()
-    erb :'tags/bubbles'
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
   end
 end
--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -1,13 +1,30 @@
 
 feature 'view links' do
+  before(:each) do
+    Link.create(url: 'http://www.bbc.co.uk', title: 'BBC', tags: [Tag.first_or_create(name: 'education')])
+    Link.create(url: 'http://www.plastic.com', title: 'Plastics', tags: [Tag.first_or_create(name: 'materials')])
+    Link.create(url: 'http://www.music.com', title: 'Music and Stuff', tags: [Tag.first_or_create(name: 'music')])
+    Link.create(url: 'http://www.music.com', title: 'Mountains', tags: [Tag.first_or_create(name: 'nature')])
+  end
+
+
   scenario 'see links on homepage' do
     #binding.pry
-    Link.create(url: 'http://www.google.com', title: 'Google')
     visit '/links'
     expect(page.status_code).to eq 200
 
     within 'ul#links' do
-     expect(page).to have_content('Google')
+     expect(page).to have_content('Plastics')
+    end
+  end
+
+
+  scenario 'user can filter links by tag' do
+    visit '/tags/nature' do
+      expect(page).not_to have_content('BBC')
+      expect(page).not_to have_content('Plastics')
+      expect(page).not_to have_content('Music and Stuff')
+      expect(page).to have_content('Mountains')
     end
   end
 end
