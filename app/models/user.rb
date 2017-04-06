53886638e1f4a9bf33f2bd46afe3e2e6e21ad330
Simba14 <seanblundell@gmail.com>
2017-04-06 18:57:23 +0100

    Password confirmation validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ require 'bcrypt'
 class User
 
   include DataMapper::Resource
+  attr_reader :password
+  attr_accessor :password_confirmation
 
   property :id, Serial
   property :email, String
@@ -12,6 +14,7 @@ class User
   property :password_digest, Text
 
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -7,13 +7,18 @@ feature 'sign up' do
     expect(User.first.email).to eq 'cheese@gmail.com'
   end
 
-  scenario 'user confirms password' do
-    visit '/users/new'
-    expect(page.status_code).to eq 200
-    fill_in :email, with: 'cheese@gmail.com'
-    fill_in :password, with: 'brie'
-    fill_in :password_confirmation, with: 'brye'
-    click_button 'Sign Up Now'
-    expect(User.first).to eq nil
+  scenario 'requires a matching confirmation password' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
   end
+
+  def sign_up(email: 'cheese@gmail.com',
+              password: '12345678',
+              password_confirmation: '12345678')
+    visit '/users/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign Up Now'
+  end
+
 end
