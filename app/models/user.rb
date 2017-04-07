fba4c786da11fd750f7ee13de38a7afc32ddd173
Simba14 <seanblundell@gmail.com>
2017-04-07 14:35:43 +0100

    Preventing duplicate registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ require 'dm-postgres-adapter'
 require 'bcrypt'
 
 class User
-  
+
   include DataMapper::Resource
   attr_reader :password
   attr_accessor :password_confirmation
 
   property :id, Serial
-  property :email, String
-
+  property :email, String, format: :email_address, required: true, unique: true
   property :password_digest, Text
 
   def password=(password)
@@ -19,4 +18,5 @@ class User
   end
 
   validates_confirmation_of :password
+  validates_format_of :email, as: :email_address
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -10,7 +10,23 @@ feature 'sign up' do
   scenario 'requires a matching confirmation password' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq('/users')
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
+  end
+
+  scenario 'cannot sign up without an email address' do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+  end
+
+  scenario 'cannot sign up with an invalid email address' do
+    expect { sign_up(email: "invalid@email") }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email has an invalid format')
+  end
+
+  scenario 'cannot sign up with existing email address' do
+    sign_up
+    expect { sign_up }.to_not change(User, :count)
+    expect(page).to have_content('Email is already taken')
   end
 
   def sign_up(email: 'cheese@gmail.com',
